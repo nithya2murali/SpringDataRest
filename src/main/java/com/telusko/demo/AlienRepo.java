4fa6be7ca8863fa4098cf19a8582578b52e263c3
nithya2murali <nithyadon@gmail.com>
2018-10-21 16:25:28 +0100

    code to branch

--- src/main/java/com/telusko/demo/AlienRepo.java
+++ src/main/java/com/telusko/demo/AlienRepo.java
@@ -6,7 +6,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.telusko.demo.model.Alien;
 @RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
 public interface AlienRepo extends JpaRepository<Alien,Integer>
-
+//first comiit
+//change to branch
 {
 
 }
